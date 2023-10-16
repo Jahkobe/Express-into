084f1781e91a47aaf0576db95b6d0196469a0476
Jacob Pursell <jacobpursell@Jacobs-MBP.lan>
2023-10-16 15:27:36 -0500

    adding some requests

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const express = require("express");
 const app = express();
 // console.dir(app);
 
-app.use((req, res) => {
-    console.log("We got a new request!")
-    // 
-    res.send({band: "Slightly Stoopid!"});
+// app.use((req, res) => {
+//     console.log("We got a new request!")
+//     // 
+//     res.send({band: "Slightly Stoopid!"});
+// })
 
+app.get('/', (req, res) => {
+    res.send("home, let me go hooomme, home is wherever im with you!")
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    // console.log(req.params)
+    const {subreddit} = req.params;
+    res.send(`<h1>Checking out the album: ${subreddit}!</h1>`)
+})
+
+app.get('/music', (req, res) => {
+    res.send("Excuse me while i kiss the sky!")
+})
+
+app.post('/music', (req, res) => {
+    res.send("Post request to /music!")
+})
+
+app.get('/games', (req, res) => {
+    res.send("It's a me Mario!");
 })
 
 app.listen(3000, () => {
